2d7bede04236b90bab88c071e7467251314bf509
Rohan Acharya <rohanacharya38@gmail.com>
2023-01-16 17:54:56 +0545

    added build script for gcc windows

--- code/builder.c
+++ code/builder.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const char *bash_string= "# build script fors SDL in bash using gcc\n\
-#!/bin/bash\n\
-if [ ! -d \"../bin\" ]; then\n\
-  mkdir \"../bin\"\n\
-fi\n\
-cd ../bin\
-source_name=\"../code/main.c ../code/sorts.c\"\n\
-executable_name=\"sort_viz\"\n\
-# Compile the program\n\
-gcc  $source_name -w -lm -lSDL2 -o $executable_name\n\
-\n\
-# Run the program\n\
-./sort_viz\n\
-";
-
-const char *pwsh_string="if(!(Test-Path(\"..\\bin\")))\n\
+#ifdef _WIN32
+#ifdef _MSC_VER
+const char *pwsh_string = "if(!(Test-Path(\"..\\bin\")))\n\
 {\n\
         New-Item -Itemtype Directory \"..\\bin\"\n\
 }\n\
@@ -33,6 +20,65 @@ if(!(Test-Path(\"SDL2.dll\")))\n\
 }\n\
 cl  $source_name /Fe$executable_name $compiler_flags /I$include_path /link /LIBPATH:$lib_path $libraries /NODEFAULTLIB:msvcrt.lib /SUBSYSTEM:CONSOLE\n\
 ./sort_viz.exe";
+#elif __GNUC__
+const char *gcc_win_str = "#download sdl2 for gcc\n\
+if(!(Test-Path(\"..\\gcc\")))\n\
+{\n\
+New-Item -Itemtype Directory \"..\\gcc\"\n\
+$SDL2 = \"https://www.libsdl.org/release/SDL2-devel-2.0.12-mingw.tar.gz\"\n\
+Push-Location ..\\gcc\n\
+New-Item -Itemtype Directory \"lib\"\n\
+New-Item -Itemtype Directory \"include\"\n\
+#curl SDL2 download link\n\
+Invoke-WebRequest -Uri $SDL2 -OutFile \"SDL2.tar.gz\"\n\
+#extract SDL2\n\
+tar -xvf SDL2.tar.gz\n\
+#copy SDL2.dll to bin\n\
+Copy-Item \"SDL2-2.0.12\\x86_64-w64-mingw32\\bin\\SDL2.dll\" .\\lib\n\
+#copy lib files to lib folder\n\
+Copy-Item \"SDL2-2.0.12\\x86_64-w64-mingw32\\lib\\*.lib\" .\\lib\n\
+Copy-Item \"SDL2-2.0.12\\x86_64-w64-mingw32\\lib\\*.la\" .\\lib\n\
+Copy-Item \"SDL2-2.0.12\\x86_64-w64-mingw32\\lib\\*.a\" .\\lib\n\
+#copy include files to include folder\n\
+Copy-Item \"SDL2-2.0.12\\x86_64-w64-mingw32\\include\\SDL2\" .\\include\n\
+Pop-Location\n\
+}\n\
+if(!(Test-Path(\"..\\bin\")))\n\
+{\n\
+        New-Item -Itemtype Directory \"..\\bin\"\n\
+}\n\
+if((Test-Path(\"..\\include\\SDL2\")))\n\
+{\n\
+        Remove-Item -Recurse -Force \"..\\include\\SDL2\"\n\
+}\n\
+Push-Location \"..\\bin\"\n\
+if(!(Test-Path(\"SDL2.dll\")))\n\
+{\n\
+        Copy-Item \"../gcc/lib/SDL2.dll\"\n\
+}\n\
+$source_name = \"..\\code\\main.c\",\"..\\code\\sorts.c\"\n\
+$executable_name = \"sort_viz.exe\"\n\
+$lib_path =\"..\\gcc\\lib\\\"\n\
+$include_path =\"..\\gcc\\include\"\n\
+#gcc compiler flags\n\
+gcc -o $executable_name $source_name -I..\\include\\gcc -L..\\lib\\ -lmingw32 -lSDL2main -lSDL2\n";
+#endif
+#elif __linux__
+const char *bash_string = "# build script fors SDL in bash using gcc\n\
+#!/bin/bash\n\
+if [ ! -d \"../bin\" ]; then\n\
+  mkdir \"../bin\"\n\
+fi\n\
+cd ../bin\
+source_name=\"../code/main.c ../code/sorts.c\"\n\
+executable_name=\"sort_viz\"\n\
+# Compile the program\n\
+gcc  $source_name -w -lm -lSDL2 -o $executable_name\n\
+\n\
+# Run the program\n\
+./sort_viz\n\
+";
+#endif
 int main()
 {
     // open sorts.h and list all function names present there
@@ -49,7 +95,7 @@ int main()
     memset(data, 0, fsize + 100);
 
     char c;
-    int rows=0;
+    int rows = 0;
     while ((c = fgetc(sortsh)) != EOF)
     {
         if (c == 'v')
@@ -83,7 +129,7 @@ int main()
     }
     fclose(sortsh);
 
-    char **functions= malloc(rows * sizeof(char *));
+    char **functions = malloc(rows * sizeof(char *));
     for (int i = 0; i < rows; i++)
     {
         functions[i] = malloc(100 * sizeof(char));
@@ -94,73 +140,81 @@ int main()
     {
         if (data[i] == '.')
         {
-            for(k=0;data[i+1]!='\n';i++,k++)
+            for (k = 0; data[i + 1] != '\n'; i++, k++)
             {
-                functions[j][k]=data[i+1];
+                functions[j][k] = data[i + 1];
             }
-            functions[j][k]='\0';
+            functions[j][k] = '\0';
             j++;
         }
-        
     }
-    
-    fsize=0;
-    int final_index=0;
-    long main_fsize=0;
-    FILE *finalptr=fopen("../code/main.c","w");
-    FILE *mainptr=fopen("../misc/main.c","r");
+
+    fsize = 0;
+    int final_index = 0;
+    long main_fsize = 0;
+    FILE *finalptr = fopen("../code/main.c", "w");
+    FILE *mainptr = fopen("../misc/main.c", "r");
     char *main_data;
     fseek(mainptr, 0L, SEEK_END);
     main_fsize = ftell(mainptr);
-    fsize+=main_fsize;
+    fsize += main_fsize;
     fseek(mainptr, 0L, SEEK_SET);
-    fsize+= strlen(data);
+    fsize += strlen(data);
     for (int i = 0; i < rows; i++)
     {
-        fsize+=strlen(functions[i]);
+        fsize += strlen(functions[i]);
     }
-    fsize+=rows*strlen("case SDL_SCANCODE_0:\ncase SDL_SCANCODE_KP_0:\nbreak;\n");
-    fsize+=strlen("int32_t switch_function(void *keycode)\n{\nSDL_Scancode key=*((SDL_Scancode *)keycode);\nswitch (key)\n\n{\ndefault:\nbreak;\n}\nthread_created=false\nreturn 0;\n}\n");
-    main_data=malloc(main_fsize+100);
-    memset(main_data, 0, main_fsize+100);
-    fread(main_data,1,main_fsize,mainptr);
+    fsize += rows * strlen("case SDL_SCANCODE_0:\ncase SDL_SCANCODE_KP_0:\nbreak;\n");
+    fsize += strlen("int32_t switch_function(void *keycode)\n{\nSDL_Scancode key=*((SDL_Scancode *)keycode);\nswitch (key)\n\n{\ndefault:\nbreak;\n}\nthread_created=false\nreturn 0;\n}\n");
+    main_data = malloc(main_fsize + 100);
+    memset(main_data, 0, main_fsize + 100);
+    fread(main_data, 1, main_fsize, mainptr);
     fclose(mainptr);
-    main_data[main_fsize]='\0';
-    char *final_str=malloc(fsize+100);
-    memset(final_str, 0, fsize+100);
-    final_index+=sprintf(final_str,"const char *display_str=\"");
+    main_data[main_fsize] = '\0';
+    char *final_str = malloc(fsize + 100);
+    memset(final_str, 0, fsize + 100);
+    final_index += sprintf(final_str, "const char *display_str=\"");
     for (int i = 0; i < rows; i++)
     {
-        final_index+=sprintf(final_str+final_index,"%d.%s\\n",i,functions[i]);
+        final_index += sprintf(final_str + final_index, "%d.%s\\n", i, functions[i]);
     }
-    final_index+=sprintf(final_str+final_index,"\";\n%s\n",main_data);
-    final_index+=sprintf(final_str+final_index,"int32_t switch_function(void *keycode)\n{\nSDL_Scancode key=*((SDL_Scancode *)keycode);\nswitch (key)\n\n{\n");
+    final_index += sprintf(final_str + final_index, "\";\n%s\n", main_data);
+    final_index += sprintf(final_str + final_index, "int32_t switch_function(void *keycode)\n{\nSDL_Scancode key=*((SDL_Scancode *)keycode);\nswitch (key)\n\n{\n");
     for (int i = 0; i < rows; i++)
     {
-        final_index+=sprintf(final_str+final_index,"case SDL_SCANCODE_%d:\ncase SDL_SCANCODE_KP_%d:\n%s(sRects, RECT_COUNT);\nbreak;\n",i,i,functions[i]);
+        final_index += sprintf(final_str + final_index, "case SDL_SCANCODE_%d:\ncase SDL_SCANCODE_KP_%d:\n%s(sRects, RECT_COUNT);\nbreak;\n", i, i, functions[i]);
     }
-    final_index+=snprintf(final_str+final_index,strlen("default:\nbreak;\n}\nthread_created=false;\nreturn 0;\n}\n"),"default:\nbreak;\n}\nthread_created=false;\nreturn 0;\n}\n");
-    final_str[final_index]='\0';
-    fwrite(final_str,1,final_index-1,finalptr);
+    final_index += snprintf(final_str + final_index, strlen("default:\nbreak;\n}\nthread_created=false;\nreturn 0;\n}\n"), "default:\nbreak;\n}\nthread_created=false;\nreturn 0;\n}\n");
+    final_str[final_index] = '\0';
+    fwrite(final_str, 1, final_index - 1, finalptr);
     fclose(finalptr);
-    
-    #ifdef _WIN32
-    FILE *script=fopen("../code/build.ps1","w");
-    fwrite(pwsh_string,1,strlen(pwsh_string),script);
+
+#ifdef _WIN32
+// for msvc compiler
+// ifdef for msvc
+#ifdef _MSC_VER
+    FILE *script = fopen("../code/build.ps1", "w");
+    fwrite(pwsh_string, 1, strlen(pwsh_string), script);
     fclose(script);
-    system("powershell -ExecutionPolicy Bypass -File ../code/build.ps1");
-    #elif __linux__
-    FILE *script=fopen("../code/build.sh","w");
-    fwrite(bash_string,1,strlen(bash_string),script);
+    system("powershell -ExecutionPolicy Bypass -File ../code/build.ps1 &");
+#elif __GNUC__
+    FILE *script = fopen("../code/buildgcc.ps1", "w");
+    fwrite(gcc_win_str, 1, strlen(gcc_win_str), script);
     fclose(script);
-    system("bash ../code/build.sh");
-    #endif
+    system("powershell -ExecutionPolicy Bypass -File ../code/buildgcc.ps1 &");
+#endif
+#elif __linux__
+    FILE *script = fopen("../code/build.sh", "w");
+    fwrite(bash_string, 1, strlen(bash_string), script);
+    fclose(script);
+    system("bash ../code/build.sh &");
+#endif
     /*
     free(data);
     free(main_data);
     free(final_str);
     */
-   //letting os free the memory is faster
-    
+    // letting os free the memory is faster
+
     return 0;
 }
